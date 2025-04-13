0c4d56d91f6e8d695ff4431b590b577776db9a86
Bombardier256 <4477617@gmail.com>
2025-04-13 17:17:22 +0300

    Install debug

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
@@ -6,4 +7,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("cinema.urls", namespace="cinema")),
+    path("__debug__", include(debug_toolbar.urls)),
 ]
