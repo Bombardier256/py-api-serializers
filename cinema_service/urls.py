1f62f36690c7d1ad75fa40e2fda5920fe841ad00
Bombardier256 <4477617@gmail.com>
2025-04-14 11:40:23 +0300

    Remove debug

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,5 +1,4 @@
 import debug_toolbar
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,5 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls", namespace="cinema")),
-    path("__debug__", include(debug_toolbar.urls)),
 ]
