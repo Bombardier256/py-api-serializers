b9fd4d63677e6ec9463c66e370d3afca3c53aa22
Bombardier256 <4477617@gmail.com>
2025-04-14 12:04:22 +0300

    Install debut add to requirements

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,4 +1,4 @@
-import debug_toolbar
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls", namespace="cinema")),
-]
+] + debug_toolbar_urls()
